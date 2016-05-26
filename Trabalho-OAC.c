6601e9d96c8899f89d3c1e8a6a624897ba17d46c
Maycon <mscrat@hotmail.com>
2016-05-26 16:54:01 -0300

    Leitura de terminal
    Programa recebe dados vindos de linhas de comando

--- Trabalho-OAC.c
+++ Trabalho-OAC.c
@@ -11,8 +11,15 @@ void tab_base(){///       8  4  2  1      8  4  2  1
 
 }
 
-int main (){
+int main( int argc, char *op[])  {
 
-
-    return 0;
+   if( argc == 3 ) {
+      printf("arquivo = %s, opcao = %s\n", op[1], op[2]);
+   }
+   else if( argc > 2 ) {
+      printf("Argumentos demais.\n");
+   }
+   else {
+      printf("Argumentos de menos.\n");
+   }
 }
