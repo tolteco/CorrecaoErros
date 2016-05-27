37c7eab59bd2d3ca4c126fe3974675d33696f0fd
Maycon <mscrat@hotmail.com>
2016-05-27 11:48:20 -0300

    Testes de entradas e atualizações para funções

---
+++ Compiler.bat
@@ -0,0 +1,3 @@
+"D:\Arquivos de Programas\Codeblocks\MinGW\bin\mingw32-gcc.exe" -c H:\Git\OACPratica\Trabalho-OAC.c -o H:\Git\OACPratica\Trabalho-OAC.o
+"D:\Arquivos de Programas\Codeblocks\MinGW\bin\mingw32-g++.exe" -o H:\Git\OACPratica\Trabalho-OAC.exe H:\Git\OACPratica\Trabalho-OAC.o
+if %errorlevel%==0 Trabalho-OAC.exe
--- Trabalho-OAC.c
+++ Trabalho-OAC.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+FILE *ARQ;
+char arquivo[50];
+
 void tab_base(){///       8  4  2  1      8  4  2  1
     int base[12][4] = { { 0, 0, 0, 1 }, { 0, 0, 1, 0 },
                         { 0, 0, 1, 1 }, { 0, 1, 0, 0 },
@@ -11,15 +14,38 @@ void tab_base(){///       8  4  2  1      8  4  2  1
 
 }
 
-int main( int argc, char *op[])  {
+int leitura(){
+    int N;
+    printf("Entrei em leitura = arquivo %s\n", arquivo);
+    ARQ = fopen(arquivo,"r");
+    fscanf (ARQ, "%i\n", &N);
+    printf("---%i\n", N);
+    return 0;
+}
 
-   if( argc == 3 ) {
-      printf("arquivo = %s, opcao = %s\n", op[1], op[2]);
-   }
-   else if( argc > 2 ) {
-      printf("Argumentos demais.\n");
-   }
-   else {
-      printf("Argumentos de menos.\n");
-   }
+int escrita(){
+    printf("Entrei em escrita\n");
+    return 0;
+}
+
+int main( int argc, char *op[])  {
+    int A = 0;
+    if(argc == 3) {
+        printf("arquivo = %s, opcao = %s\n", op[1], op[2]);
+        strcpy(arquivo, op[1]);
+        if (strcmp(op[2], "-r")==0){
+            A = leitura();
+        } else if (strcmp(op[2], "-w")==0){
+            A = escrita();
+        } else {
+            printf("Opcao invalida\n");
+        }
+    }
+    else if(argc > 2) {
+        printf("Argumentos demais.\n");
+    }
+    else {
+        printf("Argumentos de menos.\n");
+    }
+    return A;
 }
