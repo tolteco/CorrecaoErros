38a32f43b317b61efa70539ff2a541b49aac1587
Maycon <mscrat@hotmail.com>
2016-06-17 15:30:17 -0300

    Correção ao Analisador

--- MultMatriz/Analisador.c
+++ MultMatriz/Analisador.c
@@ -4,38 +4,52 @@
 FILE *IN;
 FILE *OUT;
 FILE *OUTCSV;
-int TM[5], TT[5];
+double TM[5], TT[5];
 double MM, MT, MM2, MO;
 
 int main(){
-  int O, E, A=0;
+  int l, c, E, A=0;
   IN = fopen("Log.txt","r");
+  if (IN == NULL){
+    printf("Erro na abertura do arquivo de leitura\n");
+    return 1;
+  }
   OUT = fopen("Results.txt","w");
+  if (OUT == NULL){
+    printf("Erro na abertura do arquivo de escrita de relatorio\n");
+    return 1;
+  }
   OUTCSV = fopen("Results.csv","w");
+  if (OUTCSV == NULL){
+    printf("Erro na abertura do arquivo csv\n");
+    return 1;
+  }
   fprintf(OUTCSV, ",MultConvencional,MultTransposta,ConvTransposta,TotalTransposta,Dif(Percent)\n");
   while (!feof(IN))
   {
-    fscanf (IN, "%i\n", &O);
-    fscanf (IN, "%i\n", TM[0]);
-    fscanf (IN, "%i\n", TM[1]);
-    fscanf (IN, "%i\n", TM[2]);
-    fscanf (IN, "%i\n", TM[3]);
-    fscanf (IN, "%i\n", TM[4]);
+    fscanf (IN, "%i\n", &l);
+    fscanf (IN, "%i\n", &c);
+    fscanf (IN, "%lf\n", &TM[0]);
+    fscanf (IN, "%lf\n", &TM[1]);
+    fscanf (IN, "%lf\n", &TM[2]);
+    fscanf (IN, "%lf\n", &TM[3]);
+    fscanf (IN, "%lf\n", &TM[4]);
     MM = (TM[0]+TM[1]+TM[2]+TM[3]+TM[4])/5;
-    fscanf (IN, "%i\n", TT[0]);
-    fscanf (IN, "%i\n", TT[1]);
-    fscanf (IN, "%i\n", TT[2]);
-    fscanf (IN, "%i\n", TT[3]);
-    fscanf (IN, "%i\n", TT[4]);
-    fscanf (IN, "%i\n", TM[0]);
-    fscanf (IN, "%i\n", TM[1]);
-    fscanf (IN, "%i\n", TM[2]);
-    fscanf (IN, "%i\n", TM[3]);
-    fscanf (IN, "%i\n", TM[4]);
+    fflush(stdout);
+    fscanf (IN, "%lf\n", &TT[0]);
+    fscanf (IN, "%lf\n", &TM[0]);
+    fscanf (IN, "%lf\n", &TT[1]);
+    fscanf (IN, "%lf\n", &TM[1]);
+    fscanf (IN, "%lf\n", &TT[2]);
+    fscanf (IN, "%lf\n", &TM[2]);
+    fscanf (IN, "%lf\n", &TT[3]);
+    fscanf (IN, "%lf\n", &TM[3]);
+    fscanf (IN, "%lf\n", &TT[4]);
+    fscanf (IN, "%lf\n", &TM[4]);
     MT = (TT[0]+TT[1]+TT[2]+TT[3]+TT[4])/5;
     MM2 = (TM[0]+TM[1]+TM[2]+TM[3]+TM[4])/5;
     MO = MT+MM2;
-    fprintf(OUT, "Para %ix%i\n", O, O);
+    fprintf(OUT, "Para %ix%i\n", l, c);
     fprintf(OUTCSV, "250x250,");
     fprintf(OUT, "Tempo de execucao da multipicacao convencional %lf\n", MM);
     fprintf(OUTCSV, "%lf,", MM);
